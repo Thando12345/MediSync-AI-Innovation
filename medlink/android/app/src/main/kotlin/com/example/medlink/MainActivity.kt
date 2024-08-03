5d9318f8f13920bbc605d88689b8a21764e901e5
Thando12345 <69775289+Thando12345@users.noreply.github.com>
2024-08-03 16:45:23 +0200

    DjangoCare-AI-Enhanced-Electronic-Health-Record-Platform

--- medlink/android/app/src/main/kotlin/com/example/medlink/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.medlink
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- medlink/lib/utils/constants.dart
+++
@@ -1,6 +0,0 @@
-// lib/utils/constants.dart
-
-const String apiUrl = 'https://example.com/api';
-const Color primaryColor = Colors.red;
-const Color secondaryColor = Colors.yellow;
-const Color accentColor = Colors.green;
